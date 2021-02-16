f77be533f9ce3f4f49bbd5ea0ed21f8e3fad3984
dimasiasko <70683494+dimasiasko@users.noreply.github.com>
2021-02-16 23:41:30 +0200

    last commit

---
+++ Лабораторна робота 2/Завдання 4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Завдання_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customClass = MyClass<CustomClass>.FactoryMethod();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class MyClass<T>
+    {
+        public static T FactoryMethod()
+        {
+            return Activator.CreateInstance<T>();
+        }
+    }
+    public class CustomClass
+    {
+
+    }
+}
